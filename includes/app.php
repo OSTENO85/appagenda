b945b4962e60a15da81be0b4e9d137dbb0f59d6b
OSTENO85 <111088245+OSTENO85@users.noreply.github.com>
2025-08-12 15:11:30 -0600

    Update app.php

--- includes/app.php
+++ includes/app.php
@@ -1,5 +1,4 @@
 <?php 
-<<<<<<< HEAD
 use Dotenv\Dotenv;
 use Model\ActiveRecord;
 
@@ -14,13 +13,4 @@ require 'database.php';
 
 // Conectarnos a la base de datos
 
-=======
-
-require 'funciones.php';
-require 'database.php';
-require __DIR__ . '/../vendor/autoload.php';
-
-// Conectarnos a la base de datos
-use Model\ActiveRecord;
->>>>>>> 25ce17e (nuevos cambios)
 ActiveRecord::setDB($db);
